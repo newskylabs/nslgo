ed27c06e3b3f95bdf6c2fc9c5e341ca4e58df6e5
dietrich <dietrich@newskylabs.net>
2019-03-27 22:36:33 +0100

    Added code from Listing 3.2 Using tuples to represent points of a Go board

--- nsl/go/gotypes.py
+++ nsl/go/gotypes.py
@@ -3,6 +3,7 @@
 ## ---------------------------------------------------------
 
 import enum
+from collections import namedtuple
 
 ## =========================================================
 ## class Player
@@ -15,6 +16,19 @@ class Player(enum.Enum):
     @property
     def other(self):
         return Player.black if self == Player.white else Player.white
+
+## =========================================================
+## class Point
+## ---------------------------------------------------------
+
+class Point(namedtuple('Point', 'row col')):
+    def neighbors(self):
+        return [
+            Point(self.row - 1, self.col),
+            Point(self.row + 1, self.col),
+            Point(self.row, self.col - 1),
+            Point(self.row, self.col + 1),
+        ]
     
 ## =========================================================
 ## =========================================================
