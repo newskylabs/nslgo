c748271b949231fb824b7112c6feddc881758550
dietrich <dietrich@newskylabs.net>
2019-03-28 12:36:54 +0100

    Added code from Listing 3.4: Encoding strings of stones with set

--- nsl/go/goboard_slow.py
+++ nsl/go/goboard_slow.py
@@ -6,6 +6,47 @@ import copy
 from nsl.go.gotypes import Player
 
 ## =========================================================
+## class GoString
+## ---------------------------------------------------------
+
+class GoString():  # <1>
+    """Go strings are stones that are linked by a chain of connected
+    stones of the same color.
+
+    """
+    def __init__(self, color, stones, liberties):
+        self.color = color
+        self.stones = set(stones)
+        self.liberties = set(liberties)
+
+    def remove_liberty(self, point):
+        self.liberties.remove(point)
+
+    def add_liberty(self, point):
+        self.liberties.add(point)
+
+    def merged_with(self, go_string):  # <2>
+        """Return a new Go string containing all stones in both strings.
+
+        """
+        assert go_string.color == self.color
+        combined_stones = self.stones | go_string.stones
+        return GoString(
+            self.color,
+            combined_stones,
+            (self.liberties | go_string.liberties) - combined_stones)
+
+    @property
+    def num_liberties(self):
+        return len(self.liberties)
+
+    def __eq__(self, other):
+        return isinstance(other, GoString) and \
+            self.color == other.color and \
+            self.stones == other.stones and \
+            self.liberties == other.liberties
+
+## =========================================================
 ## class Move
 ## ---------------------------------------------------------
 
