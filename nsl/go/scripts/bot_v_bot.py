1354b6be3bb36ddd2c55d1095dadd37cd8b4f270
dietrich <dietrich@newskylabs.net>
2019-04-14 17:42:02 +0200

    Moved bot_v_bot.py -> nsl/go/scripts/bot_v_bot.py

--- nsl/go/scripts/bot_v_bot.py
+++ nsl/go/scripts/bot_v_bot.py
@@ -2,18 +2,20 @@
 ## bot_v_bot.py
 ## ---------------------------------------------------------
 
-from nsl.go import agent
-from nsl.go import goboard
-from nsl.go import gotypes
-from nsl.go.utils import print_board, print_move
 import time
 
+from nsl.go import agent
+from nsl.go import goboard_slow as goboard
+from nsl.go import gotypes
+from nsl.go.utils import print_board, print_move, clear_screen
+
 ## =========================================================
-## main()
+## Main
 ## ---------------------------------------------------------
 
-def main():
-    board_size = 9
+def bot_v_bot(board_size=9, sleep=0.3):
+    """A random bot playing against another random bot.
+    """
     game = goboard.GameState.new_game(board_size)
     bots = {
         gotypes.Player.black: agent.naive.RandomBot(),
@@ -23,23 +25,29 @@ def main():
 
         # We set a sleep timer to 0.3 seconds so that bot moves aren't
         # printed too fast to observe
-        time.sleep(0.3)
+        time.sleep(sleep)
 
         # Before each move we clear the screen. This way the board is
         # always printed to the same position on the command line.
-        print(chr(27) + "[2J")
-        
+        #| print(chr(27) + "[2J")
+        clear_screen()
+
+        # Print a header
+        print('')
+        print('  nslgo')
+        print('')
+
+        # Print the board
         print_board(game.board)
+        print('')
+        
+        # Calculate and print the next move
         bot_move = bots[game.next_player].select_move(game)
         print_move(game.next_player, bot_move)
+        print('')
+        
+        # Apply the move
         game = game.apply_move(bot_move)
-
-## =========================================================
-## Main
-## ---------------------------------------------------------
-
-if __name__ == '__main__':
-    main()
 
 ## =========================================================
 ## =========================================================
