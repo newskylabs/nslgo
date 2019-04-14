0af2940424a65a7493b182d0621d0ed2604189d1
dietrich <dietrich@newskylabs.net>
2019-04-14 18:04:44 +0200

    Edited the board printing functions to allow for more beautiful output

--- nsl/go/utils.py
+++ nsl/go/utils.py
@@ -1,6 +1,7 @@
 ## =========================================================
 ## nsl/go/utils.py
 ## ---------------------------------------------------------
+import os
 
 from nsl.go import gotypes
 
@@ -21,10 +22,21 @@ STONE_TO_CHAR = {
 }
 
 ## =========================================================
+## clear_screen()
+## ---------------------------------------------------------
+
+def clear_screen():
+    """Clear the screen.
+    """
+    # Use `cls' on Windows;
+    # and `clear' on Unix systems.
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+## =========================================================
 ## print_move()
 ## ---------------------------------------------------------
 
-def print_move(player, move):
+def print_move(player, move, prefix='  '):
 
     if move.is_pass:
         move_str = 'passes'
@@ -35,13 +47,13 @@ def print_move(player, move):
     else:
         move_str = '%s%d' % (COLS[move.point.col - 1], move.point.row)
 
-    print('%s %s' % (player, move_str))
+    print('%s%s %s' % (prefix, player, move_str))
 
 ## =========================================================
 ## print_board()
 ## ---------------------------------------------------------
 
-def print_board(board):
+def print_board(board, prefix='  '):
 
     for row in range(board.num_rows, 0, -1):
         bump = " " if row <= 9 else ""
@@ -49,9 +61,9 @@ def print_board(board):
         for col in range(1, board.num_cols + 1):
             stone = board.get(gotypes.Point(row=row, col=col))
             line.append(STONE_TO_CHAR[stone])
-        print('%s%d %s' % (bump, row, ''.join(line)))
+        print('%s%s%d %s' % (prefix, bump, row, ''.join(line)))
 
-    print('    ' + '  '.join(COLS[:board.num_cols]))
+    print(prefix + '    ' + '  '.join(COLS[:board.num_cols]))
 
 ## =========================================================
 ## =========================================================
