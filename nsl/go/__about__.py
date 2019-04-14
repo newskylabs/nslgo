2ef4035e601dab26e72e502142a469b779b47220
dietrich <dietrich@newskylabs.net>
2019-04-14 18:07:21 +0200

    Added an nsl/go/__about__.py file with __package_name__, __version__, etc. infos

---
+++ nsl/go/__about__.py
@@ -0,0 +1,54 @@
+## =========================================================
+## Copyright 2019 Dietrich Bollmann
+## 
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+## 
+##      http://www.apache.org/licenses/LICENSE-2.0
+## 
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+## ---------------------------------------------------------
+
+## =========================================================
+## Package metadata
+## ---------------------------------------------------------
+
+__all__ = [
+    '__package_name__',
+    '__version__',
+    '__status__',
+    '__description__',
+    '__author__',
+    '__authors__',
+    '__maintainer__',
+    '__email__',
+    '__contact__',
+    '__copyright__',
+    '__url__',
+    '__license__',
+    '__date__',
+]
+
+__package_name__ = 'nslgo'
+__version__      = '0.0.1.dev1'
+__status__       = 'Development'
+__description__  = 'NewSkyLabs Python project {}'.format(__package_name__)
+__author__       = 'Dietrich Bollmann'
+__authors__      = [__author__]
+__maintainer__   = __author__
+__email__        = 'dietrich@newskylabs.net'
+__contact__      = __email__
+__copyright__    = 'Copyright 2019 {}'.format(__author__)
+__url__          = 'http://newskylabs.net/python/packages/{}'.format(__package_name__)
+__license__      = 'Apache License 2.0, http://www.apache.org/licenses/LICENSE-2.0'
+__date__         = '2019/04/12'
+
+## =========================================================
+## =========================================================
+
+## fin.
