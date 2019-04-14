02d25c3ee6e50de8ad5b685dc2624fe37548a76e
dietrich <dietrich@newskylabs.net>
2019-04-14 18:59:24 +0200

    A slightly more beautiful board...

--- nsl/go/utils.py
+++ nsl/go/utils.py
@@ -33,9 +33,9 @@ COLS = 'ABCDEFGHJKLMNOPQRST'
 ## ---------------------------------------------------------
 
 STONE_TO_CHAR = {
-    None: ' . ',
-    gotypes.Player.black: ' x ',
-    gotypes.Player.white: ' o ',
+    None: '.',
+    gotypes.Player.black: 'x',
+    gotypes.Player.white: 'o',
 }
 
 ## =========================================================
@@ -77,10 +77,11 @@ def print_board(board, prefix='  '):
         line = []
         for col in range(1, board.num_cols + 1):
             stone = board.get(gotypes.Point(row=row, col=col))
-            line.append(STONE_TO_CHAR[stone])
-        print('%s%s%d %s' % (prefix, bump, row, ''.join(line)))
+            line.append(' ' + STONE_TO_CHAR[stone])
+        print('%s%s%d  %s' % (prefix, bump, row, ''.join(line)))
 
-    print(prefix + '    ' + '  '.join(COLS[:board.num_cols]))
+    print('')
+    print(prefix + '     ' + ' '.join(COLS[:board.num_cols]))
 
 ## =========================================================
 ## =========================================================
