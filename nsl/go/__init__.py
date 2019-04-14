75a3a2287d0331d36d2a3b98edcc9053a9a09b73
dietrich <dietrich@newskylabs.net>
2019-04-14 18:11:09 +0200

    Edited nsl/go/__init__.py to make use of the variables like __package_name__, __version__, etc. defined in nsl/go/__about__.py

---
+++ nsl/go/__init__.py
@@ -0,0 +1,52 @@
+## =========================================================
+## Copyright 2019 Dietrich Bollmann
+## 
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+## 
+##      http://www.apache.org/licenses/LICENSE-2.0
+## 
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+## ---------------------------------------------------------
+
+from nsl.go.__about__ import (
+    __package_name__,
+    __version__,
+    __status__,
+    __description__,
+    __author__,
+    __authors__,
+    __maintainer__,
+    __email__,
+    __contact__,
+    __copyright__,
+    __url__,
+    __license__,
+    __date__,
+)
+
+__all__ = [
+    '__package_name__',
+    '__version__',
+    '__status__',
+    '__description__',
+    '__author__',
+    '__authors__',
+    '__maintainer__',
+    '__email__',
+    '__contact__',
+    '__copyright__',
+    '__url__',
+    '__license__',
+    '__date__',
+]
+
+## =========================================================
+## =========================================================
+
+## fin.
