488511cb5865d6ba7b79e997d005e03279e9d6a8
dietrich <dietrich@newskylabs.net>
2019-05-02 16:55:51 +0200

    Listing 3.22: Instantiating the Go board with a _hash value for the empty board

--- nsl/go/goboard.py
+++ nsl/go/goboard.py
@@ -66,6 +66,7 @@ class Board():
         self.num_rows = num_rows
         self.num_cols = num_cols
         self._grid = {}
+        self._hash = zobrist.EMPTY_BOARD
 
     def place_stone(self, player, point):
         assert self.is_on_grid(point)
